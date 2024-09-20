82b112b167a62b62c53eafc1f702a8025e83316d
vithun5 <rvithun99@gmail.com>
2024-09-20 16:07:01 +0530

    experience section update

--- src/container/homepage/Experience/Experience.js
+++ src/container/homepage/Experience/Experience.js
@@ -1,207 +1,87 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import "./Experience.module.scss";
-import { Typography, Box, Container } from "@mui/material";
-
+import React from "react";
+import styles from "./Experience.module.scss";
 const Experience = () => {
-  const [showSK, setShowSK] = useState(false);
-  const [showPampers, setShowPampers] = useState(false);
+  const experiences = [
+    {
+      id: 1,
+      company: "Al Rajhi Takaful (ART)",
+      role: "Full stack developer",
+      duration: "October 2023 - present ",
+      description:
+        "I worked as a frontend developer using Angular for the Al Rajhi Takaful insurance website , focusing on building responsive and user-friendly interfaces. The platform offers a wide range of insurance services, including medical, MMP, life, travel, and domestic helper coverage. My role involved optimizing performance, enhancing user experience, and ensuring seamless integration with backend APIs to deliver a smooth and intuitive customer journey across different devices",
+    },
+    {
+      id: 2,
+      company: "SK-II GEN3",
+      role: "FrontEnd Developer",
+      duration: "February 2023 - October 2023",
+      description:
+        "I worked as a frontend developer using ReactJS for the SK-II beauty product website, which has over four versions in different languages, including Singaporean, English, Chinese, Taiwanese, and Japanese. I was responsible for maintaining and optimizing the frontend across all versions of the site.",
+    },
+    {
+      id: 3,
+      company: "P&G PAMPERS US DEVOPS",
+      role: "ReactJS Developer",
+      duration: "February 2022 - 2023",
+      description:
+        "I contributed as a frontend developer on the P&G Pampers project in the US, utilizing ReactJS and Contentful to enhance user experience. Additionally, I actively participated in team meetings and collaborated on bug fixes to improve overall project quality.",
+    },
+  ];
   return (
     <div id="experience">
-      <section className="experience-section">
-        <Container className="Experience-container">
-          <Box sx={{ textAlign: "center", marginTop: "33px" }}>
-            <Typography
-              variant="h4"
-              sx={{
-                textTransform: "uppercase",
-                fontWeight: "500",
-                letterSpacing: "2px",
-                fontFamily: "Roboto",
+      <div className={styles["timeline-card"]}>
+        <div
+          className={styles["timeline-container"]}
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "50px",
+            flexDirection: "column",
+            width: "65%",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          <div
+            style={{
+              width: "2px",
+              height: "85%",
+              backgroundColor: "#16423C ",
+              position: "absolute",
+              right: "50%",
+            }}
+            className={styles["centerline"]}
+          ></div>
+          {experiences.map((exp) => (
+            <div
+              key={exp.id}
+              className={styles["timeline-item"]}
+              style={{
+                display: "flex",
+                alignSelf: exp.id % 2 === 0 ? "flex-end" : "flex-start",
               }}
             >
-              Experience
-            </Typography>
-            <Typography
-              sx={{
-                fontSize: "16px",
-                lineHeight: "30px",
-                color: "gray",
-                marginBottom: "50px",
-              }}
-            >
-              A variety of Experiences.
-            </Typography>
-          </Box>
-
-          {/* Project 1 */}
-          <>
-            <Box className="center-line"></Box>
-            <Box className="project-icon">
-              <i
-                class="bi bi-briefcase-fill"
+              <div
+                className={styles["card"]}
                 style={{
-                  fontSize: "23px",
-                  color: "white",
-                  position: "absolute",
-                  left: "32%",
-                  top: "20%",
+                  width: "340px",
+                  height: "fit-content",
+                  borderRadius: "16px",
                 }}
-              ></i>
-            </Box>
-            <Box className="project-details">
-              <Typography className="title1">SK-II GEN3</Typography>
-              <Typography className="subtitle1">FrontEnd Developer</Typography>
-              {!showSK && (
-                <i
-                  class="bi bi-caret-down-square-fill bg-FF6969"
-                  style={{
-                    float: "right",
-                    fontSize: "20px",
-                    position: "relative",
-                    bottom: "20px",
-                    backgroundColor: "FF6969",
-                  }}
-                  onClick={() => setShowSK(true)}
-                ></i>
-              )}
-              {showSK && (
-                <Typography className="description1">
-                  It is a Beauty product Website, Where there are more than 4
-                  websites for this product which contains different languages
-                  like singapore, English, Chinese, Taiwan, Japan, Tokyo etc. I
-                  worked as a Frontend developer to maintain all the frontend
-                  work of all websites.
-                </Typography>
-              )}
-            </Box>
-            <Typography
-              sx={{
-                textAlign: "right",
-                position: "absolute",
-                right: "421px",
-                fontSize: "19px",
-                letterSpacing: "4px",
-                fontFamily: "Montserrat",
-              }}
-              style={{ top: showSK ? "325px" : "327px" }}
-              className="project1-date"
-            >
-              April 2023 - present
-            </Typography>
-          </>
-          <>
-            <Box className="center-line"></Box>
-            <Box className="project-icon">
-              <i
-                class="bi bi-briefcase-fill"
-                style={{
-                  fontSize: "23px",
-                  color: "white",
-                  position: "absolute",
-                  left: "32%",
-                  top: "20%",
-                }}
-              ></i>
-            </Box>
-            <Box className="project-details">
-              <Typography className="title1">SK-II GEN3</Typography>
-              <Typography className="subtitle1">FrontEnd Developer</Typography>
-              {!showSK && (
-                <i
-                  class="bi bi-caret-down-square-fill bg-FF6969"
-                  style={{
-                    float: "right",
-                    fontSize: "20px",
-                    position: "relative",
-                    bottom: "20px",
-                    backgroundColor: "FF6969",
-                  }}
-                  onClick={() => setShowSK(true)}
-                ></i>
-              )}
-              {showSK && (
-                <Typography className="description1">
-                  It is a Beauty product Website, Where there are more than 4
-                  websites for this product which contains different languages
-                  like singapore, English, Chinese, Taiwan, Japan, Tokyo etc. I
-                  worked as a Frontend developer to maintain all the frontend
-                  work of all websites.
-                </Typography>
-              )}
-            </Box>
-            <Typography
-              sx={{
-                textAlign: "right",
-                position: "absolute",
-                right: "421px",
-                fontSize: "19px",
-                letterSpacing: "4px",
-                fontFamily: "Montserrat",
-              }}
-              style={{ top: showSK ? "325px" : "327px" }}
-              className="project1-date"
-            >
-              April 2023 - present
-            </Typography>
-          </>
-
-          <>
-            <Box
-              className="completedProject-icon"
-              style={{ bottom: showSK ? "132px" : "248px" }}
-            >
-              <i
-                class="bi bi-check2-all"
-                style={{
-                  fontSize: "23px",
-                  color: "white",
-                  position: "absolute",
-                  left: "32%",
-                  top: "20%",
-                }}
-              ></i>
-            </Box>
-            <Box className="project-details2">
-              <Typography className="title2"> P&G PAMPERS US DEVOPS</Typography>
-              <Typography className="subtitle2">React.JS Developer</Typography>
-              {!showPampers && (
-                <i
-                  class="bi bi-caret-down-square-fill bg-FF6969"
-                  style={{
-                    float: "right",
-                    fontSize: "20px",
-                    position: "relative",
-                    bottom: "20px",
-                    backgroundColor: "FF6969",
-                  }}
-                  onClick={() => setShowPampers(true)}
-                ></i>
-              )}
-              {showPampers && (
-                <Typography className="description2">
-                  We are a small team of software engineers and testers with
-                  varied experience and academic backgrounds.We develop and
-                  maintain the website and I worked as a frontend developer.
-                </Typography>
-              )}
-              <Typography
-                sx={{
-                  position: "relative",
-                  right: "572px",
-                  fontSize: "19px",
-                  letterSpacing: "4px",
-                  fontFamily: "Montserrat",
-                }}
-                style={{ bottom: showPampers ? "114px" : "35px" }}
-                className="project2-date"
               >
-                February 2022 - 2023
-              </Typography>
-            </Box>
-          </>
-        </Container>
-      </section>
+                <div className="card-body">
+                  <h5 className="card-title">{exp.duration}</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">
+                    {exp.company}
+                  </h6>
+                  <p className="card-text">{exp.role}</p>
+                  <p className="card-text">{exp.description}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
